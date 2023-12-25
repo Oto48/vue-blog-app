cb4c2eb0c994c915fce8005981c7c9d921555955
oto48 <otchokhonelidze4@gmail.com>
2023-12-25 22:13:02 +0400

    add new form styles and validation

--- src/views/AddBlog/AddBlog.vue
+++ src/views/AddBlog/AddBlog.vue
@@ -9,30 +9,7 @@
       class="file"
       :class="{ 'drag-over': dragging }"
     >
-      <svg
-        width="40"
-        height="40"
-        viewBox="0 0 40 40"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g id="vuesax/bulk/folder-add">
-          <g id="folder-add">
-            <path
-              id="Vector"
-              opacity="0.4"
-              d="M36.233 15.7333H3.33301V10.7C3.33301 6.63333 6.63301 3.33333 10.6997 3.33333H14.583C17.2997 3.33333 18.1497 4.21667 19.233 5.66667L21.5663 8.76667C22.083 9.45 22.1497 9.55 23.1163 9.55H27.7663C31.7163 9.53333 35.083 12.1333 36.233 15.7333Z"
-              fill="#5D37F3"
-            />
-            <path
-              id="Vector_2"
-              d="M36.6497 18.0667C36.6163 17.25 36.483 16.4833 36.233 15.7333H3.33301V27.75C3.33301 32.6667 7.33301 36.6667 12.2497 36.6667H27.7497C32.6663 36.6667 36.6663 32.6667 36.6663 27.75V18.45C36.6663 18.3333 36.6663 18.1833 36.6497 18.0667ZM24.1663 27.0833H21.3497V30C21.3497 30.6833 20.783 31.25 20.0997 31.25C19.4163 31.25 18.8497 30.6833 18.8497 30V27.0833H15.833C15.1497 27.0833 14.583 26.5167 14.583 25.8333C14.583 25.15 15.1497 24.5833 15.833 24.5833H18.8497V21.6667C18.8497 20.9833 19.4163 20.4167 20.0997 20.4167C20.783 20.4167 21.3497 20.9833 21.3497 21.6667V24.5833H24.1663C24.8497 24.5833 25.4163 25.15 25.4163 25.8333C25.4163 26.5167 24.8497 27.0833 24.1663 27.0833Z"
-              fill="#5D37F3"
-            />
-          </g>
-        </g>
-      </svg>
-
+      <FileSvg />
       <p>
         ჩააგდეთ ფაილი აქ ან
         <span @click="openFileInput" class="text-bold">აირჩიეთ ფაილი</span>
@@ -47,16 +24,58 @@
     <div class="input_container">
       <div>
         <label for="author">ავტორი *</label>
-        <input type="text" name="author" v-model="formData.author" />
+        <input
+          type="text"
+          name="author"
+          v-model="formData.author"
+          @input="validateAuthor"
+          @blur="updateAuthorBorder"
+          :class="authorError ? 'error' : ''"
+        />
+        <p
+          v-for="validation in authorValidation"
+          :key="validation.text"
+          :style="{ color: validation.color }"
+        >
+          {{ validation.text }}
+        </p>
       </div>
       <div>
         <label for="title">სათაური *</label>
-        <input type="text" name="title" v-model="formData.title" />
+        <input
+          type="text"
+          name="title"
+          v-model="formData.title"
+          @input="validateTitle"
+          @blur="updateTitleBorder"
+          :class="titleError ? 'error' : ''"
+        />
+        <p
+          v-for="validation in titleValidation"
+          :key="validation.text"
+          :style="{ color: validation.color }"
+        >
+          {{ validation.text }}
+        </p>
       </div>
     </div>
     <div>
       <label for="description">აღწერა *</label>
-      <input type="text" name="description" v-model="formData.description" />
+      <input
+        type="text"
+        name="description"
+        v-model="formData.description"
+        @input="validateDescription"
+        @blur="updateDescriptionBorder"
+        :class="descriptionError ? 'error' : ''"
+      />
+      <p
+        v-for="validation in descriptionValidation"
+        :key="validation.text"
+        :style="{ color: validation.color }"
+      >
+        {{ validation.text }}
+      </p>
     </div>
     <div class="input_container">
       <div>
@@ -65,11 +84,31 @@
           type="date"
           name="publish_date"
           v-model="formData.publish_date"
+          @input="validateDate"
+          @blur="updateDateBorder"
+          :class="dateError ? 'error' : ''"
         />
+        <p
+          v-for="validation in dateValidation"
+          :key="validation.text"
+          :style="{ color: validation.color }"
+        >
+          {{ validation.text }}
+        </p>
       </div>
       <div>
         <label for="categories">კატეგორია *</label>
-        <input type="text" name="categories" v-model="formData.categories" />
+
+        <select name="categories" v-model="formData.categories">
+          <option value="" disabled>აირჩიეთ კატეგორია</option>
+          <option
+            v-for="category in categories"
+            :key="category.id"
+            :value="category.id"
+          >
+            {{ category.title }}
+          </option>
+        </select>
       </div>
     </div>
     <div>
@@ -82,23 +121,43 @@
 
 <script>
 import axios from "axios";
+import FileSvg from "../../assets/svg/FolderSvg";
 
 export default {
   name: "AddBlog",
 
+  components: {
+    FileSvg: FileSvg,
+  },
+
   data() {
     return {
       formData: {
-        title: "test test",
-        description: "test test",
+        author: "",
+        title: "",
         image: null,
-        author: "გელა გელაშვილი",
-        publish_date: "2023-11-19",
+        description: "",
+        publish_date: "",
         categories: "[1,2,3,4,5]",
         email: "gigagiorgadze@redberry.ge",
       },
 
+      categories: [],
+
+      authorValidation: [
+        { text: "მინიმუმ 4 სიმბოლო" },
+        { text: "მინიმუმ ორი სიტყვა" },
+        { text: "მხოლოდ ქართული სიმბოლოები" },
+      ],
+      titleValidation: [{ text: "მინიმუმ 2 სიმბოლო" }],
+      descriptionValidation: [{ text: "მინიმუმ 2 სიმბოლო" }],
+      dateValidation: [{ text: "გთხოვთ აირჩიოთ თარიღი" }],
+
       dragging: false,
+      authorError: false,
+      titleError: false,
+      descriptionError: false,
+      dateError: false,
     };
   },
 
@@ -135,11 +194,69 @@ export default {
       console.log(event);
       this.formData.image = event;
     },
+
+    validateAuthor() {
+      const author = this.formData.author.trim();
+
+      const words = author.split(/\s+/);
+      const nonSpaceAuthor = author.replace(/\s+/g, "");
+      const georgianCharsRegex = /^[ა-ჰ\s]+$/;
+
+      this.authorValidation[0].color =
+        nonSpaceAuthor.length >= 4 ? "green" : "red";
+      this.authorValidation[1].color = words.length >= 2 ? "green" : "red";
+      this.authorValidation[2].color = georgianCharsRegex.test(author)
+        ? "green"
+        : "red";
+    },
+
+    updateAuthorBorder() {
+      const author = this.formData.author.trim();
+      this.authorError =
+        author.length < 4 ||
+        author.split(/\s+/).length < 2 ||
+        !/^[ა-ჰ\s]+$/.test(author);
+      this.validateAuthor();
+    },
+
+    validateTitle() {
+      const title = this.formData.title.trim();
+      this.titleValidation[0].color = title.length >= 2 ? "green" : "red";
+    },
+
+    updateTitleBorder() {
+      this.titleError = this.formData.title.trim().length < 2;
+      this.validateTitle();
+    },
+
+    validateDescription() {
+      const description = this.formData.description.trim();
+      this.descriptionValidation[0].color =
+        description.length >= 2 ? "green" : "red";
+    },
+
+    updateDescriptionBorder() {
+      this.descriptionError = this.formData.description.trim().length < 2;
+      this.validateDescription();
+    },
+
+    validateDate() {
+      const date = this.formData.publish_date;
+
+      this.dateValidation[0].color = date ? "green" : "red";
+      this.dateError = !date;
+    },
+
+    updateDateBorder() {
+      if (!this.isDateFocused) {
+        this.dateError = !this.formData.publish_date;
+      }
+    },
   },
 
   mounted() {
     axios
-      .get("https://api.blog.redberryinternship.ge/api/blogs", {
+      .get("https://api.blog.redberryinternship.ge/api/categories", {
         headers: {
           Accept: "application/json",
           Authorization:
@@ -147,7 +264,9 @@ export default {
         },
       })
       .then((response) => {
-        console.log(response.data);
+        const items = response.data;
+        this.categories = items.data;
+        console.log(this.categories);
       });
   },
 };
